f8b67da38695cbf54298bc3f5a17117c13fc4222
Alexander Zapata <alexander.zapata@duke.edu>
2018-09-06 14:12:08 -0400

    Update video.py
    Code refactoring.

--- video.py
+++ video.py
@@ -5,96 +5,95 @@ import time
 import timeit
 import socket
 
-#ser = serial.Serial('/dev/ttyUSB0')
-TCP_IP = '127.0.0.1'
+#Setting variables
+TCP_IP = '127.0.0.1' #Currently set to localhost
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-
+FRAME_HEIGHT = 1080
+FRAME_WIDTH = 1920
 options = {
     'model': 'cfg/tiny-yolo-voc-1c.cfg',
     'load': 1375,
     'threshold': 0.1,
 }
+colors = [tuple(np.random.randint(low=0, high=256, size=3)) for _ in range(10)]
 
+#Opening socket connection on given IP and Port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+#Instantiating TensorFlow net
 tfnet = TFNet(options)
-colors = [tuple(255 * np.random.rand(3)) for _ in range(10)]
 
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+#Starting video capturing
+capture = cv2.VideoCapture(0) #Beginning video capture with camera with device-index 0
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
 
+#Helper methods
+def determine_direction(movement_vectors):
+    result_strings = ("", "")
+    if(movement_vectors[0] < 0):
+        result_strings[0] = "go left {}".format(movement_vectors[0])
+    else:
+        result_strings[0] = "go right {}".format(movement_vectors[0])
+    if(movement_vectors[1] < 0):
+        result_strings[1] = "go up {}".format(movement_vectors[1])
+    else:
+        result_strings[1] = "go down {}".format(movement_vectors[1])
 
-
+#Main program loop
 while True:
-    stime = time.time()
-    start = timeit.timeit()
-    ret, frame = capture.read()
-    if ret:
+    #Read frame-date from VideoCapture object
+    capture_successful, frame = capture.read()
+    if capture_successful:
         results = tfnet.return_predict(frame)
         for color, result in zip(colors, results):
-            tl = (result['topleft']['x'], result['topleft']['y'])
-            #print("topleft x {}".format(result['topleft']['x']))
-            #print("topleft y {}".format(result['topleft']['y']))
+            #Setting variables for the top-left and bottom-right points (x, y) of result border-box
+            borderbox_topleft = (result['topleft']['x'], result['topleft']['y'])
+            borderbox_bottomright = (result['bottomright']['x'], result['bottomright']['y'])
 
-            midpointX = (result['topleft']['x'] + result['bottomright']['x'])/2 
+            #Calculating midpoint on frame
+            midpointX = (borderbox_topleft[0] + borderbox_bottomright[0])/2
             midpointY = (result['topleft']['y'] + result['bottomright']['y'])/2
+            borderbox_midpoint = (midpointX, midpointY)
 
-            distX = abs(midpointX - 600)
-            distY = abs(midpointY - 400)
-
-
-            br = (result['bottomright']['x'], result['bottomright']['y'])
-            #print("bottomright x {}".format(result['bottomright']['x']))
-            #print("bottomright y {}".format(result['bottomright']['y']))
+            '''
+            Calculating movement distances (in pixels) required to center border-box on frame.
+            We can try to calibrate movement velocities left and right based on these numbers.
+            There would of course be some range of acceptable valuables for x and y.
+            '''
+            frame_center = (np.floor(FRAME_WIDTH/2), np.floor(FRAME_HEIGHT/2))
+            movement_vectors = (frame_center[0] - borderbox_midpoint[0], frame_center[1] - borderbox_midpoint[1])
 
             print("midpoint ({},{})".format(midpointX, midpointY))
+            print("movement-vectors: {}".format(movement_vectors))
 
-            #s.send("x:{}, y:{}".format(midpointX, midpointY).encode())
-            s.send("{}/{}/{}".format(midpointX, midpointY).encode())
-
-
-            end = timeit.timeit()
-            print("latency???? in milliseconds {}".format((end - start)*1000))
-
+            c.send('HTTP/1.0 200 OK\n')
+            c.send('Content-Type: text/html\n')
+            c.send('\n') # header and body should be separated by additional newline
+            c.send("<html><body><h1>{}</h1></body></html>".format(movement_vectors))
 
             label = result['label']
             confidence = result['confidence']
             text = '{}: {:.0f}%'.format(label, confidence * 100)
-            frame = cv2.rectangle(frame, tl, br, color, 5)
-            frame = cv2.putText(
-                frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-            if(midpointX < 600):
-                frame = cv2.putText(
-                frame, "go right {}".format(distX), (20, 155), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            else:
-                frame = cv2.putText(
-                frame, "go left {}".format(distX), (20, 155), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            frame = cv2.rectangle(frame, borderbox_topleft, borderbox_bottomright, color, 5)
+            frame = cv2.putText(frame, text, borderbox_topleft, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+            text_directions = determine_direction(movement_vectors)
+            frame = cv2.putText(frame, text_directions[0], (20, 155), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            frame = cv2.putText(frame, text_directions[1], (20, 55), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
-            if(midpointY < 400):
-                frame = cv2.putText(
-                frame, "go down {}".format(distY), (20, 55), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            else:
-                frame = cv2.putText(
-                frame, "go up {}".format(distY), (20, 55), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            if(movement_vectors[0] < 25 and movement_vectors[1] < 25):
+                frame = cv2.putText(frame, "LANDING PAD CENTERED", (20, 300), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
-            if(distX < 25 and distY < 25):
-                frame = cv2.putText(
-                frame, "LANDING PAD CENTERED", (20, 300), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
-
-            #print('{} -- {}'.format(label, confidence))
         cv2.imshow('frame', frame)
-        #print('FPS {:.1f}'.format(1 / (time.time() - stime)))
-        #print('[INFO] elapsed time: {:.2f}'.format(time.time() - stime))
-        #print(label)
     else:
+        #No midpoint found due to failed frame-capture
         print("midpoint X: n, Y: n")
-        s.send(b"/n/n/n")
-        
+        s.send(b"n/n/n")
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+s.close()
 capture.release()
 cv2.destroyAllWindows()
